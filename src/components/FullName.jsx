f0afc5475281494d78ef5c0292abc6db1452a0c1
kadershaikh <kadershaikh666@gmail.com>
2024-05-03 15:47:09 +0530

    initial

--- src/components/FullName.jsx
+++ src/components/FullName.jsx
@@ -1,45 +1,49 @@
 import React, { useState } from 'react';
 
 export default function FullName() {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [fullName, setFullName] = useState('');
-
-  const handleFirstNameChange = (event) => {
-    setFirstName(event.target.value);
-  };
-
-  const handleLastNameChange = (event) => {
-    setLastName(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (firstName && lastName) {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [fullName, setFullName] = useState('');
+    const [bothFieldsFilled, setBothFieldsFilled] = useState(false);
+  
+    const handleFirstNameChange = (event) => {
+      setFirstName(event.target.value);
+      setBothFieldsFilled(event.target.value && lastName);
+    };
+  
+    const handleLastNameChange = (event) => {
+      setLastName(event.target.value);
+      setBothFieldsFilled(firstName && event.target.value);
+    };
+  
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      if (bothFieldsFilled) {
         setFullName(`${firstName} ${lastName}`);
       } else {
         setFullName('');
       }
-  };
+    };
+  
+    return (
+      <div>
+        <h1>Full Name Display</h1>
+        <form onSubmit={handleSubmit}>
+          <label>
+            First Name:
+            <input type="text" value={firstName} onChange={handleFirstNameChange} required />
+          </label>
+          <br />
+          <label>
+            Last Name:
+            <input type="text" value={lastName} onChange={handleLastNameChange} required />
+          </label>
+          <br />
+          <button type="submit" disabled={!bothFieldsFilled}>Submit</button>
+        </form>
+        {fullName && <p>Full Name: {fullName}</p>}
+      </div>
+    );
 
-  return (
-    <div>
-      <h1>Full Name Display</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          First Name:
-          <input type="text" value={firstName} onChange={handleFirstNameChange} required/>
-        </label>
-        <br />
-        <label>
-          Last Name:
-          <input type="text" value={lastName} onChange={handleLastNameChange} required/>
-        </label>
-        <br />
-        <button type="submit">Submit</button>
-      </form>
-      <p>Full Name: {fullName}</p>
-    </div>
-  );
 }
 
